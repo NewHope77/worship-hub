987eb849becbedb43be3db58ad0318f1c0404174
Pauls <pauls@MacBook-Pro-Pauls.local>
2026-09-05 23:24:42 +0200

    Updates

---
+++ assets/pdf.worker.min-3muHQ-is.js
@@ -0,0 +1,1 @@
+var e=`/worship-hub/assets/pdf.worker.min-Dswkl-cV.mjs`;export{e as default};
---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/worship-hub/sw.js', { scope: '/worship-hub/' })})}
